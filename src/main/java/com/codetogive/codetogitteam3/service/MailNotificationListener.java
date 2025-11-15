b7b1d4d13496cdbdc3d0fdaff5594ac8d9c01a83
George Fam <george.fam@famcode.net>
2025-11-15 08:52:14 -0500

    feat: Enable asynchronous email sending

--- src/main/java/com/codetogive/codetogitteam3/service/MailNotificationListener.java
+++ src/main/java/com/codetogive/codetogitteam3/service/MailNotificationListener.java
@@ -7,9 +7,9 @@ import com.codetogive.codetogitteam3.repository.SubscriptionRepository;
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -26,6 +26,7 @@ public class MailNotificationListener {
     private final DonationEventRepository eventRepo;
     private final JavaMailSender mailSender;
 
+    @Async
     @TransactionalEventListener
     public void onDonationEventPublished(DonationEventService.DonationEventPublished evt) {
         DonationEvent ev = eventRepo.findById(evt.getEventId()).orElse(null);
